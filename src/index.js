3583bea3c530b4352f8159d6140e52515628dab4
Eri Nagashima <es6.ukf@gmail.com>
2020-10-04 22:12:26 +0900

    Update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 class Character {
-  constructor() {
+  constructor(character) {
+    this.name = character.name;
+    this.hp = character.hp;
+    this.mp = character.mp;
+    // this.initialHP = character.initialHP;
+    // this.initialMP = character.initialMP;
+    // this.offensePower = character.offensePower;
+    // this.defencePower = character.defencePower;
   }
 
-  showStatus() {
+  showStatus(character) {
     /* 
       キャラクターの名前、HP、MPを表示する。
     */
+    const mainField = document.getElementById('main');
+    mainField.textContent = `Name: ${character.name}, HP: ${character.hp}, MP: ${character.mp}`;
   }
 
   attack(defender) {
     /*
       キャラクターが死んでいる場合は攻撃出来ないので、それを表示する。
       死んでいない場合は相手に与えたダメージを表示。
-      相手が死んだ場合は相手に与えたダメージと死んだことを表示する。 
+      相手が死んだ場合は相手に与えたダメージと死んだことを表示する。
     */
   }
 
@@ -23,66 +32,70 @@ class Character {
     */
   }
 }
+const chara1 = new Character({
+  name: 'Eri',
+  hp: '100',
+  mp: '50',
+});
+chara1.showStatus(chara1);
 
-class Sorcerer extends Character {
-  constructor() {
-    
-  }
+// class Sorcerer extends Character {
+//   constructor() {}
 
-  healSpell(target) {
-    /* 
-      回復魔法は3のMPを消費する。
-      相手のHPを15回復する。
-      魔法使いが死んでいる場合はその旨を表示する。
-      相手が死んでいる場合は回復が出来ないためその旨を表示する。
-      MPが足りない場合はその旨を表示する。
-    */
-  }
+//   healSpell(target) {
+//     /*
+//       回復魔法は3のMPを消費する。
+//       相手のHPを15回復する。
+//       魔法使いが死んでいる場合はその旨を表示する。
+//       相手が死んでいる場合は回復が出来ないためその旨を表示する。
+//       MPが足りない場合はその旨を表示する。
+//     */
+//   }
 
-  fireSpell(target) {
-    /* 
-      攻撃魔法は2のMPを消費する。
-      相手に10のダメージを与える。
-      魔法使いが死んでいる場合はその旨を表示する。
-      相手が死んでいる場合は攻撃が出来ないためその旨を表示する。
-      MPが足りない場合はその旨を表示する。
-    */
-  }
-}
+//   fireSpell(target) {
+//     /*
+//       攻撃魔法は2のMPを消費する。
+//       相手に10のダメージを与える。
+//       魔法使いが死んでいる場合はその旨を表示する。
+//       相手が死んでいる場合は攻撃が出来ないためその旨を表示する。
+//       MPが足りない場合はその旨を表示する。
+//     */
+//   }
+// }
 
-{
-  const fighter = new Character({
-    name: '武道家',
-    hp: 40,
-    mp: 0,
-    offensePower: 15,
-    defencePower: 10
-  })
-  const sorcerer = new Sorcerer({
-    name: '魔法使い',
-    hp: 25,
-    mp: 10,
-    offensePower: 8,
-    defencePower: 10
-  })
-  const monster = new Character({
-    name: 'モンスター',
-    hp: 60,
-    mp: 0,
-    offensePower: 30,
-    defencePower: 10
-  })
+// {
+//   const fighter = new Character({
+//     name: '武道家',
+//     hp: 40,
+//     mp: 0,
+//     offensePower: 15,
+//     defencePower: 10,
+//   });
+//   const sorcerer = new Sorcerer({
+//     name: '魔法使い',
+//     hp: 25,
+//     mp: 10,
+//     offensePower: 8,
+//     defencePower: 10,
+//   });
+//   const monster = new Character({
+//     name: 'モンスター',
+//     hp: 60,
+//     mp: 0,
+//     offensePower: 30,
+//     defencePower: 10,
+//   });
 
-  fighter.attack(monster);
-  sorcerer.attack(monster);
-  monster.attack(sorcerer);
-  fighter.attack(monster);
-  sorcerer.healSpell(sorcerer);
-  monster.attack(fighter);
-  fighter.attack(monster);
-  sorcerer.fireSpell(monster);
-  monster.attack(fighter);
-  fighter.showStatus();
-  sorcerer.showStatus();
-  monster.showStatus();
-}
+//   fighter.attack(monster);
+//   sorcerer.attack(monster);
+//   monster.attack(sorcerer);
+//   fighter.attack(monster);
+//   sorcerer.healSpell(sorcerer);
+//   monster.attack(fighter);
+//   fighter.attack(monster);
+//   sorcerer.fireSpell(monster);
+//   monster.attack(fighter);
+//   fighter.showStatus();
+//   sorcerer.showStatus();
+//   monster.showStatus();
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,15 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: './bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     hot: true,
     hotOnly: true,
     watchContentBase: true,
     watchOptions: {
-      ignored: /node_modules/
-    }
+      ignored: /node_modules/,
+    },
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 };
